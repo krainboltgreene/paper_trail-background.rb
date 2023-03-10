1494979af681e208fb4268b07fd6835aaa20de1b
Brett Wilkins <brett.wilkins@smartsheet.com>
2023-03-10 16:14:04 +1000

    Put rspec in correct location for being able to require it from outside the gem

--- lib/paper_trail-background/rspec.rb
+++ lib/paper_trail-background/rspec.rb
@@ -1,6 +1,6 @@
 require 'ar_after_transaction'
 
-require_relative 'paper_trail/background/rspec_helpers'
+require_relative '../paper_trail/background/rspec_helpers'
 
 RSpec.configure do |config|
   config.include PaperTrail::Background::RSpecHelpers::InstanceMethods
